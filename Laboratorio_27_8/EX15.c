28cd4e6b7deff9441a93915b609bad59f0b5a2c8
Sofia de Abreu <92234623+sofiamasha@users.noreply.github.com>
2025-08-27 14:10:22 +0000

    Cada codigo em sua respectiva pasta

---
+++ Laboratorio_27_8/EX15.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main(){
+    int cedula, valor;
+printf("Vc quer sacar quanto?: ");
+scanf("%d", &valor);
+while(valor!=0){
+
+printf("Vc quer sacar quanto?: ");
+scanf("%d", &valor);
+cedula= valor 
+g
+}
+}
