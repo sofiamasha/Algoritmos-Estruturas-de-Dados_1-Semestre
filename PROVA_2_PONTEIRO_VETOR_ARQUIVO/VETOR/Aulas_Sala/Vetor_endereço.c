39400f87a332a7752dee56329c89c29cc4e71b95
Sofia de Abreu <92234623+sofiamasha@users.noreply.github.com>
2025-09-29 15:08:29 +0000

    Vetor

---
+++ PROVA_2_PONTEIRO_VETOR_ARQUIVO/VETOR/Aulas_Sala/Vetor_endereço.c
@@ -0,0 +1,18 @@
+
+
+#include <stdio.h>
+
+int main()
+{
+   int vetor [10];
+   for(int i=0; i<10; i++){
+       printf("Digite o %d numero: ", i+1);
+       scanf("%d", &vetor[i]);
+       
+   }
+   for(int i=0; i<10; i++){
+       printf("(%p) %d\t", &vetor[i], vetor[i]);
+   }
+   printf("\n");
+    return 0;
+}
