0d237efe1d93dc1e2de9241e54a4152ce82f03d8
Sofia de Abreu <92234623+sofiamasha@users.noreply.github.com>
2025-10-01 12:06:47 +0000

    Exercvicio 2

--- 2.c
+++ 2.c
@@ -20,3 +20,21 @@ int main()
 
     return 0;
 }
+
+/*Ou com função: 
+
+#include <stdio.h>
+void soma(int *n1, int *n2){
+    int somar = *n1+*n2;
+    printf("A soma eh: %d\n", somar);
+}
+int main()
+{
+    int n,u;
+    printf("Digite o 1 e 2 numero: ");
+    scanf("%d %d", &n, &u);
+  soma(&n, &u);
+    
+
+    return 0;
+}*/
